64693793a0ad032c058701e18eba6fe614e97de8
Abishek <abishek.ramdas@liquid-markets.com>
2025-05-28 15:31:23 +0200

    Create a link between two nodes

--- graph.c
+++ graph.c
@@ -1,6 +1,8 @@
 #include "graph.h"
 #include "gluethread/glthread.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * @brief Create a new graph data structure and initialize name.
  *
@@ -37,10 +39,58 @@ node_t* create_graph_node(graph_t *graph, const char *node_name){
     return nodep;
 }
 
-void insert_link_between_two_nodes(node_t *node1,
+/**
+ * @brief Create a link and connect it between nodes.
+ *
+ * @param  node1: pointer to first node
+ * @param  node2: pointer to second node
+ * @param  from_if_name: interface name on first node
+ * @param  to_if_name: interface name on second node
+ * @param  cost: cost of the link 
+ * @return  0: on success
+ *         <0: fail
+ *
+ */
+int insert_link_between_two_nodes(node_t *node1,
                                    node_t *node2,
                                    char *from_if_name,
                                    char *to_if_name,
                                    unsigned int cost){
-    
+
+    link_t *new_link = calloc(1, sizeof(link_t));
+    if(new_link == NULL){
+        perror("calloc:");
+        return -1;
+    }
+
+    int node1_free_if = get_free_if_idx_from_node(node1);
+    if(node1_free_if < 0){
+        printf("Unable to find free interface on node1\n");
+        return -1;
+    }
+
+    int node2_free_if = get_free_if_idx_from_node(node2);
+    if(node2_free_if < 0){
+        printf("Unable to find free interface on node2\n");
+        return -1;
+    }
+
+    new_link->if1.link = new_link;
+    new_link->if2.link = new_link;
+    new_link->cost = cost;
+
+    interface_t *if1 = &new_link->if1;
+    strncpy(if1->interface_name, from_if_name, IF_NAME_SIZE);
+    if1->link = new_link;
+    if1->attached_node = node1;
+
+    interface_t *if2 = &new_link->if2;
+    strncpy(if2->interface_name, to_if_name, IF_NAME_SIZE);
+    if2->link = new_link;
+    if2->attached_node = node2;
+
+    node1->interfaces[node1_free_if] = if1;
+    node2->interfaces[node2_free_if] = if2;
+
+    return 0; // success
 }
--- graph.h
+++ graph.h
@@ -43,13 +43,60 @@ typedef struct interface_ {
 
 // Link connects two interfaces
 typedef struct link_ {
-    interface_t *if1; ///< interfaces in this link
-    interface_t *if2;
-    unsigned int cost; ///< cost of this link
+    interface_t if1; ///< interfaces in this link
+    interface_t if2;
+    unsigned int cost; ///< cost of this link, not used
 } link_t;
 
 
 // Functions
 extern graph_t* create_new_graph(const char *topology_name);
 extern node_t* create_graph_node(graph_t *graph, const char *node_name);
+
+
+// Helper functions
+/**
+ * @brief Get the neighbour node connected across the link on a given interface.
+ *
+ * @param  interface* : pointer to interface to get nbr on
+ * @return node * : pointer to neighbour node across link
+ *         NULL   : no neighbour found
+ *
+ */
+static inline node_t * get_nbr_node(interface_t *interface){
+    link_t *if_link = interface->link;
+    interface_t *nbr_link_if;
+    if(if_link == NULL){
+        return NULL;
+    }
+
+    if(&(if_link->if1) == interface){
+        nbr_link_if = &(if_link->if2);
+    } else if (&(if_link->if2) == interface) {
+        nbr_link_if = &(if_link->if1);
+    } else {
+        return NULL;
+    }
+
+    return nbr_link_if->attached_node;
+}
+
+/**
+ * @brief Get the index of a free interface in input node
+ *
+ * @param  *node  : pointer to node
+ * @return int >=0: index of free interface in node
+ *              <0: no free interfaces
+ *
+ */
+static inline int get_free_if_idx_from_node(node_t *node){
+    for(int i=0; i<MAX_INTERFACES_PER_NODE; i++){
+        if(node->interfaces[i] == NULL){
+            return i;
+        }
+    }
+    return -1; // failed to find empty interface in current node
+}
+
+
 #endif
