3f23d5c9d107b439df669788737718cf02cc580b
Abishek <abi01shek@gmail.com>
2025-06-12 20:14:29 +0200

    Adding command parser

--- Makefile
+++ Makefile
@@ -1,17 +1,23 @@
 CC=gcc
 CFLAGS=-g -Wall -Wno-unused-variable -Wno-unused-but-set-variable -Werror=return-type -Wextra -Wpedantic
 LDFLAGS=
+LIBS = -lpthread -L CommandParser -lcli
 SRCS = gluethread/glthread.c net.c graph.c topologies.c main.c
 OBJS = $(SRCS:.c=.o)
 EXECUTABLE = main
 
 all: $(EXECUTABLE)
+	(cd CommandParser; make)
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
-$(EXECUTABLE): $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) -o $@
+$(EXECUTABLE): $(OBJS) CommandParser/libcli.a
+	$(CC) $(CFLAGS) $(OBJS) -o $@ $(LIBS)
+
+CommandParser/libcli.a:
+	(cd CommandParser; make)
 
 clean:
 	rm -f $(OBJS) $(EXECUTABLE)
+	(cd CommandParser; make clean)
--- main.c
+++ main.c
@@ -1,16 +1,56 @@
+#include "CommandParser/clistd.h"
 #include "graph.h"
 #include <stdio.h>
 #include <string.h>
+#include "CommandParser/cmdtlv.h"
+#include "CommandParser/libcli.h"
 
 extern graph_t * build_first_topo();
+graph_t *topo = NULL;
+
+#define CMDCODE_SHOW_TOPOLOGY 1 ///< Show the topology of the network
+static int
+show_topo_callback(param_t *param,
+                   ser_buff_t *tlv_buf,
+                   op_mode enable_or_disable){
+    int CMDCODE = -1;
+    CMDCODE = EXTRACT_CMD_CODE(tlv_buf);
+    switch(CMDCODE){
+        case CMDCODE_SHOW_TOPOLOGY:
+            dump_graph(topo);
+            break;
+        default:
+            ;
+    }
+    return 0;
+}
 
 int main(){
 
-    graph_t *first_topo = build_first_topo();
-    dump_graph(first_topo);
-    return 0; // success
-    node_t *node = get_node_by_node_name(first_topo, "R0_re");
-    if(node != NULL){
-        dump_node(node);
+    graph_t *topo = build_first_topo();
+    /* node_t *node = get_node_by_node_name(topo, "R0_re"); */
+    /* if(node != NULL){ */
+    /*     dump_node(node); */
+    /* } */
+
+    init_libcli();
+    param_t *show   = libcli_get_show_hook();
+    param_t *debug  = libcli_get_debug_hook();
+    param_t *config = libcli_get_config_hook();
+    param_t *clear  = libcli_get_clear_hook();
+    param_t *run    = libcli_get_run_hook();
+
+    //CMD1: show topology
+    {
+        static param_t topology;
+        init_param(&topology, CMD, "topology", show_topo_callback, 0, INVALID, 0,
+                   "Show the topology of the net network");
+        set_param_cmd_code(&topology, CMDCODE_SHOW_TOPOLOGY);
+        libcli_register_param(show, &topology);
     }
+
+    support_cmd_negation(config);
+    /*Do not add any param in config command tree after above line*/
+    start_shell();
+    return 0;
 }
